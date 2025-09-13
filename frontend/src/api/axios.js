1ad85150f2c4b0809a4b633a177d19dc69091651
Vaibhav Maheshwari <vaibhavmaheshwari6490@gmail.com>
2025-09-13 16:02:22 +0530

    backend prod connection

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api", // change port if your backend is different
-  withCredentials: true, // optional (for auth/cookies later)
+  baseURL: `${import.meta.env.VITE_API_URL}/api`,
+  withCredentials: true, // keep this if you're using cookies/auth
 });
 
 export default api;
