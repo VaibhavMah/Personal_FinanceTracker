46d2d5e42f35205fce61962983cdd637cfdee0df
Vaibhav Maheshwari <vaibhavmaheshwari6490@gmail.com>
2025-09-13 16:27:29 +0530

    CORS

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -2,7 +2,10 @@ import axios from "axios";
 
 const api = axios.create({
   baseURL: `${import.meta.env.VITE_API_URL}/api`,
-  withCredentials: true, // keep this if you're using cookies/auth
+  withCredentials: true,
+   headers: {
+    "Content-Type": "application/json",
+  } // keep this if you're using cookies/auth
 });
 
 export default api;
